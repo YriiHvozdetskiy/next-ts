113972457462543c31f6b147c45d9b3c2e23b865
rasty <hvozdetskiyyrii@gmail.com>
2023-08-16 21:41:51 +0300

    add button

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,14 @@
+import {Button} from "@/components/ui/button";
 import Link from "next/link";
 
-import {Button} from "@/components/ui/button";
-
-export default function HomePage() {
+export default function AboutPage() {
    return (
       <>
          <section>
-            HomePage
+            AboutPage
             {/*якщо потрібна силка asChild*/}
-            <Button asChild>
-               <Link href={'/about'}>About</Link>
+            <Button asChild variant={'destructive'}>
+               <Link href="/">Home</Link>
             </Button>
          </section>
       </>
